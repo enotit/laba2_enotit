f7467cc5567f608452d29c968d49b65f6ffe61ea
enotit <43219870+enotit@users.noreply.github.com>
2022-12-26 16:29:41 +0300

    Лаба 2

--- laba2_enotit/laba2_enotit.cpp
+++ laba2_enotit/laba2_enotit.cpp
@@ -1,13 +1,13 @@
-﻿
-#include <iostream>
+﻿#include <iostream>
 #include <string>
 #include <fstream>
 #include <Windows.h>
-#include <locale>
+//#include <locale>
 
 using namespace std;
 
-struct Disk {
+struct Disk
+{
 	string title;
 	string director;
 	string genre;
@@ -16,6 +16,37 @@ struct Disk {
 	string company;
 	float imdb;
 };
+
+void write_file(Disk arr[], int array_len, string file_name)
+{
+	ofstream fout(file_name);
+	fout << array_len;
+	for (int i = 0; i < array_len; i++)
+	{
+		Disk disk = arr[i];
+		fout << disk.title << endl;
+		fout << disk.director << endl;
+		fout << disk.genre << endl;
+		fout << disk.duration << endl;
+		fout << disk.year << endl;
+		fout << disk.company << endl;
+		fout << disk.imdb << endl;
+	}
+
+	fout.close();
+};
+
+void print_disk(Disk disk)
+{
+	cout << "Фильм: '" << disk.title << "'\n";
+	cout << "Режиссёр: '" << disk.director << "'\n";
+	cout << "Жанр: '" << disk.genre << "'\n";
+	cout << "Длительность(мин): '" << disk.duration << "'\n";
+	cout << "Год выпуска: '" << disk.year << "'\n";
+	cout << "Компания: '" << disk.company << "'\n";
+	cout << "Оценка: '" << disk.imdb << "'\n";
+	cout << "=========================\n";
+}
 
 Disk *disks;
 
@@ -27,50 +58,59 @@ Disk read_file(ifstream &file)
 	getline(file, disk.genre);
 	file >> disk.duration;
 	file >> disk.year;
+	file.get();
 	getline(file, disk.company);
 	file >> disk.imdb;
+	file.get();
+	print_disk(disk);
 	return disk;
 };
 
-
-void print_film(Disk disk) {
-	cout << "\n Название: ";
-};
 int main()
 {
-	setlocale(LC_ALL, "rus");
+	setlocale(LC_ALL, "Russian");
+	// SetConsoleOutputCP(1251); // FIXME: почему то не работает( 
+	//SetConsoleCP(1251); // FIXME: почему то не работает( 
 	ifstream infile;
 	infile.open("text.txt");
 
 	int count;
 	infile >> count;
-	std::cout << count;
+	cout << "Прочитал:" << count << "\n";
 	disks = new Disk[count];
+	infile.get();
 
 	for (int i = 0; i < count; i++)
 		disks[i] = read_file(infile);
 	infile.close();
 
-
-
 	string director;
-	std::cin >> director;
+	cout << "Выведите режиссёра:";
+	getline(cin, director);
 	int m_year = INT_MAX;
 	Disk result;
-	for (int i = 0; i < count; i++) {
-		if (disks[i].year < m_year && director == disks[i].director) {
+	cout << director << "\n";
+	for (int i = 0; i < count; i++)
+	{
+
+		if (disks[i].year < m_year && director == disks[i].director)
+		{
 			result = disks[i];
 			m_year = result.year;
 		}
 	}
-	std::cout << result.title << std::endl;
+	cout << result.title << " в " << m_year << endl;
 
 	string genre;
-	std::cin >> genre;
+	cout << "Выведите жанр:";
+	getline(cin, genre);
+	cout << "\n";
 
-	for (int i = 0; i < count; i++) {
-		if (disks[i].imdb >= 8.0 && genre == disks[i].genre) {
-			std::cout << disks[i].title << std::endl;
+	for (int i = 0; i < count; i++)
+	{
+		if (disks[i].imdb >= 8.0 && genre == disks[i].genre)
+		{
+			cout << disks[i].title << endl;
 		}
 	}
 
